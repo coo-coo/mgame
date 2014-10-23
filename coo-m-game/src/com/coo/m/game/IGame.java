e13bddd0f97b30a1ef1564e158fb1762f4d717e3
boqing.shen <boqing.shen@AP-CHN-LP100068.internal.sungard.corp>
2014-10-23 16:53:29 +0800

    1

--- coo-m-game/src/com/coo/m/game/IGame.java
+++ coo-m-game/src/com/coo/m/game/IGame.java
@@ -7,13 +7,13 @@ public interface IGame {
 	int MISSION_FAIL = 20; // 任务失败
 	int MISSION_TIMEOUT = 21; // 任务超时
 	int MISSION_GIVEUP = 22; // 任务放弃
-	
+
 	int MISSION_SCORE_ADD = 3; // 增加分值
-	
+
 	int GAME_INIT = 0; // 游戏初始化
 	int GAME_OVER = 9; // 游戏结束
 	int GAME_SCORE = 100; // 游戏成绩
-	
+
 	/**
 	 * 游戏初始化
 	 */
@@ -33,13 +33,15 @@ public interface IGame {
 	 * 任务过关:多关模式
 	 */
 	public void onMissionSuccess();
-	
+
 	/**
 	 * 任务完成:单关模式
 	 */
 	public void onMissionComplete();
-	
-	
+
+	/**
+	 * 任务完成:单关模式的分数加
+	 */
 	public void onMissionScoreAdd(int score);
 
 	/**
